d179f037efafa1a54bd9b0d52dadf8fc9ab34ac4
tracer-NEMU2020 <tjuics2020@163.com>
2023-08-27 19:37:41 -0700

    >  compile NEMU 3022244233 skyeyecontroller Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:37:41 up 1 day,  9:06,  1 user,  load average: 0.00, 0.07, 0.05 ea50f441577bdbb47b08215d89aff0bb5093de10

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -99,16 +99,21 @@ static bool make_token(char *e)
 		{
 			if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0)
 			{
-				 char *substr_start = e + position;
+				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
-				 Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
 				 * to record the token in the array `tokens'. For certain types
 				 * of tokens, some extra actions should be performed.
 				 */
+				int j;
+				for (j = 0; j < 32; j++)
+				{
+					tokens[nr_token].str[j] = '\0';
+				}
 				if (substr_len < 32)
 					switch (rules[i].token_type)
 					{
