cb2ad609e86fd6cb406227683100e7910bb00e00
SkyEyeController <luanboyue@foxmail.com>
2023-09-06 12:08:02 -0700

    历经了9个小时的高强度奋战找到了pop-template.h的严重错误

--- nemu/src/cpu/exec/process/pop-template.h
+++ nemu/src/cpu/exec/process/pop-template.h
@@ -1,15 +1,21 @@
-#include "cpu/exec/template-start.h"
+#include<cpu/exec/template-start.h>
 
 #define instr pop
-
+//指令pop 58+n，pop_r_v
 static void do_execute()
 {
-    OPERAND_W(op_src,MEM_R(reg_l(R_ESP)));
-    MEM_W(reg_l(R_ESP),0);
-    reg_l(R_ESP) += DATA_BYTE;
+    if(DATA_BYTE==2)
+    {
+        swaddr_write((DATA_TYPE)op_src->reg,2,MEM_R(R_ESP));
+        reg_l(R_ESP)+=DATA_BYTE;
+    }
+    else
+    {
+        swaddr_write((DATA_TYPE)op_src->reg,4,MEM_R(R_ESP));
+        reg_l(R_ESP)+=DATA_BYTE;
+    }
     print_asm_template1();
 }
-make_instr_helper(r)
-make_instr_helper(rm)
-
-#include "cpu/exec/template-end.h"
+make_instr_helper(r);
+make_instr_helper(rm);
+#include<cpu/exec/template-end.h>
