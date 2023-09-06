e43fce355bc213edcfee2a009c315de63d001c16
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-06 04:11:31 -0700

    >  compile NEMU 3022244233 skyeyecontroller Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:11:31 up 4 days, 14:16,  1 user,  load average: 0.09, 0.04, 0.01 984a8eb39745a702ab241c83961a5c99581717d8

--- nemu/src/cpu/exec/control/cmp-template.h
+++ nemu/src/cpu/exec/control/cmp-template.h
@@ -3,12 +3,18 @@
 #define instr cmp
 
 static void do_execute () {
-	DATA_TYPE DEST = op_dest -> val - op_src -> val;
+	DATA_TYPE ret = op_dest -> val - op_src -> val;
+    cpu.eflags.ZF = !ret;
     cpu.eflags.CF = op_dest -> val < op_src -> val;
+    cpu.eflags.SF = ret >> ((DATA_BYTE << 3) - 1);
     int tmp1 = (op_dest -> val) >> ((DATA_BYTE << 3) - 1);
     int tmp2 = (op_src -> val) >> ((DATA_BYTE << 3) - 1);
-    update_eflags_pf_zf_sf(DEST);
     cpu.eflags.OF = (tmp1 != tmp2 && tmp2 == cpu.eflags.SF);
+    ret ^= ret >> 4;
+    ret ^= ret >> 2;
+    ret ^= ret >> 1;
+    ret &= 1;
+    cpu.eflags.PF = !ret;
     print_asm_template2(); 
 }
 
