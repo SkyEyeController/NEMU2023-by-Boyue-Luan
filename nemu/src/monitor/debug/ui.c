409bc0c7a4da64a0886c48f5ecca899ca01bcb29
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-07 04:14:02 -0700

    >  compile NEMU 3022244233 skyeyecontroller Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  04:14:02 up 5 days, 14:18,  1 user,  load average: 0.00, 0.01, 0.00 7dd28156e93340c3a10c7232652bb5a50721cb8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -128,6 +128,32 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_bt(char *args){
+	const char* find_fun_name(uint32_t eip);
+	struct{
+	swaddr_t prev_ebp;
+	swaddr_t ret_addr;
+	uint32_t args[4];
+	}sf;
+
+	uint32_t ebp = cpu.ebp;
+	uint32_t eip = cpu.eip;
+	int i;
+	i = 0;
+	while(ebp != 0){
+	sf.args[0] = swaddr_read(ebp + 8, 4);
+	sf.args[1] = swaddr_read(ebp + 12, 4);
+	sf.args[2] = swaddr_read(ebp + 16, 4);
+	sf.args[3] = swaddr_read(ebp + 20, 4);
+	
+	printf("#%d 0x%08x in %s (0x%08x 0x%08x 0x%08x 0x%08x)\n", i, eip, find_fun_name(eip), sf.args[0],sf.args[1], sf.args[2], sf.args[3]);
+	i ++;
+	eip = swaddr_read(ebp + 4, 4);
+	ebp = swaddr_read(ebp, 4);
+	}
+	return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -145,8 +171,8 @@ static struct {
 	{ "x", "Examine memory", cmd_x },
         { "p", "Evaluate the value of expression", cmd_p },
 	{ "w", "Set watchpoint", cmd_w },
-	{ "d", "Delete watchpoint", cmd_d }
-
+	{ "d", "Delete watchpoint", cmd_d },
+	{ "bt", "Print stack frame chain", cmd_bt},
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
