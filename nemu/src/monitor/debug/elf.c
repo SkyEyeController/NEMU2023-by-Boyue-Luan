9db3e156d63e670a8eaaedbfb342172427d9477c
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-07 22:30:22 -0700

    >  compile NEMU 3022244233 skyeyecontroller Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:30:22 up 6 days,  8:34,  1 user,  load average: 0.08, 0.12, 0.14 8a1a21eea27185a6940e02a920509bd66b3866

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -8,7 +8,8 @@ static char *strtab = NULL;
 static Elf32_Sym *symtab = NULL;
 static int nr_symtab_entry;
 
-void load_elf_tables(int argc, char *argv[]) {
+void load_elf_tables(int argc, char *argv[])
+{
 	int ret;
 	Assert(argc == 2, "run NEMU with format 'nemu [program]'");
 	exec_file = argv[1];
@@ -25,17 +26,16 @@ void load_elf_tables(int argc, char *argv[]) {
 	char magic[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
 
 	/* Check ELF header */
-	assert(memcmp(elf->e_ident, magic, 4) == 0);		// magic number
-	assert(elf->e_ident[EI_CLASS] == ELFCLASS32);		// 32-bit architecture
-	assert(elf->e_ident[EI_DATA] == ELFDATA2LSB);		// littel-endian
-	assert(elf->e_ident[EI_VERSION] == EV_CURRENT);		// current version
-	assert(elf->e_ident[EI_OSABI] == ELFOSABI_SYSV || 	// UNIX System V ABI
-			elf->e_ident[EI_OSABI] == ELFOSABI_LINUX); 	// UNIX - GNU
-	assert(elf->e_ident[EI_ABIVERSION] == 0);			// should be 0
-	assert(elf->e_type == ET_EXEC);						// executable file
-	assert(elf->e_machine == EM_386);					// Intel 80386 architecture
-	assert(elf->e_version == EV_CURRENT);				// current version
-
+	assert(memcmp(elf->e_ident, magic, 4) == 0);	  // magic number
+	assert(elf->e_ident[EI_CLASS] == ELFCLASS32);	  // 32-bit architecture
+	assert(elf->e_ident[EI_DATA] == ELFDATA2LSB);	  // littel-endian
+	assert(elf->e_ident[EI_VERSION] == EV_CURRENT);	  // current version
+	assert(elf->e_ident[EI_OSABI] == ELFOSABI_SYSV || // UNIX System V ABI
+		   elf->e_ident[EI_OSABI] == ELFOSABI_LINUX); // UNIX - GNU
+	assert(elf->e_ident[EI_ABIVERSION] == 0);		  // should be 0
+	assert(elf->e_type == ET_EXEC);					  // executable file
+	assert(elf->e_machine == EM_386);				  // Intel 80386 architecture
+	assert(elf->e_version == EV_CURRENT);			  // current version
 
 	/* Load symbol table and string table for future use */
 
@@ -53,9 +53,11 @@ void load_elf_tables(int argc, char *argv[]) {
 	assert(ret == 1);
 
 	int i;
-	for(i = 0; i < elf->e_shnum; i ++) {
-		if(sh[i].sh_type == SHT_SYMTAB && 
-				strcmp(shstrtab + sh[i].sh_name, ".symtab") == 0) {
+	for (i = 0; i < elf->e_shnum; i++)
+	{
+		if (sh[i].sh_type == SHT_SYMTAB &&
+			strcmp(shstrtab + sh[i].sh_name, ".symtab") == 0)
+		{
 			/* Load symbol table from exec_file */
 			symtab = malloc(sh[i].sh_size);
 			fseek(fp, sh[i].sh_offset, SEEK_SET);
@@ -63,8 +65,9 @@ void load_elf_tables(int argc, char *argv[]) {
 			assert(ret == 1);
 			nr_symtab_entry = sh[i].sh_size / sizeof(symtab[0]);
 		}
-		else if(sh[i].sh_type == SHT_STRTAB && 
-				strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0) {
+		else if (sh[i].sh_type == SHT_STRTAB &&
+				 strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0)
+		{
 			/* Load string table from exec_file */
 			strtab = malloc(sh[i].sh_size);
 			fseek(fp, sh[i].sh_offset, SEEK_SET);
@@ -80,4 +83,32 @@ void load_elf_tables(int argc, char *argv[]) {
 
 	fclose(fp);
 }
+uint32_t look_up_symtab(char *sym, bool *success)
+{
+	int i;
+	for (i = 0; i < nr_symtab_entry; i++)
+	{
+		uint8_t type = ELF32_ST_TYPE(symtab[i].st_info);
+		if ((type == STT_FUNC || type == STT_OBJECT) && strcmp(strtab + symtab[i].st_name, sym) == 0)
+		{
+			*success = true;
+			return symtab[i].st_value;
+		}
+	}
+	*success = false;
+	return 0;
+}
 
+const char *find_fun_name(uint32_t eip)
+{
+	static const char not_found[] = "???";
+	int i;
+	for (i = 0; i < nr_symtab_entry; i++)
+	{
+		if (ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC && eip >= symtab[i].st_value && eip < symtab[i].st_value + symtab[i].st_size)
+		{
+			return strtab + symtab[i].st_name;
+		}
+	}
+	return not_found;
+}
