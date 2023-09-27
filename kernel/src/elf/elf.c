40c1145d51c6c8b696dc48b3dca3ac8827fc3852
tracer-NEMU2020 <tjuics2020@163.com>
2023-09-27 08:08:32 -0700

    >  compile NEMU 3022244233 skyeyecontroller Linux ubuntu 5.4.0-150-generic #167~18.04.1-Ubuntu SMP Wed May 24 00:51:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:08:32 up 4 days,  8:28,  1 user,  load average: 0.18, 0.17, 0.14 91ea3199d77325235bf1376e62f74a8ff5bddab4

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 #include <string.h>
 #include <elf.h>
-#include <stdio.h>
+
 #define ELF_OFFSET_IN_DISK 0
 
 #ifdef HAS_DEVICE
@@ -16,8 +16,7 @@ void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 void create_video_mapping();
 uint32_t get_ucr3();
 
-uint32_t loader()
-{
+uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
@@ -29,43 +28,40 @@ uint32_t loader()
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
 
-	elf = (void *)buf;
+	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x464c457f;//拜托了，小端序哎
+	const uint32_t elf_magic = 0X464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
-	// panic("please implement me");
-	int i = 0;
-	ph = (void *)(buf + elf->e_phoff);
-	for (; i < elf->e_phnum; ++i, ++ph)
-	{
+	//panic("please implement me");
+	int i;
+	ph = (void*)(buf + elf -> e_phoff);
+	for(i = 0; i < elf -> e_phnum; i ++) {
 		/* Scan the program header table, load each segment into memory */
-		if (ph->p_type == PT_LOAD)
-		{
-			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
-			/* TODO: read the content of the segment from the ELF file
+		if(ph->p_type == PT_LOAD) {
+
+			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-#ifdef HAS_DEVICE
-			ide_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
-#else
-			ramdisk_read((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
-#endif
-			/* TODO: zero the memory region
+			
+			ph -> p_vaddr = mm_malloc(ph -> p_vaddr,ph -> p_memsz);
+			
+			ramdisk_read((void*)(ph -> p_vaddr),ph -> p_offset,ph -> p_filesz);
+			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			memset((void*)(ph -> p_vaddr + ph -> p_filesz),0,ph -> p_memsz - ph -> p_filesz);
+
+			ph ++;
+
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
-			extern uint32_t brk;
+			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if (brk < new_brk)
-			{
-				brk = new_brk;
-			}
+			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
 		}
 	}
@@ -74,7 +70,7 @@ uint32_t loader()
 
 #ifdef IA32_PAGE
 	mm_malloc(KOFFSET - STACK_SIZE, STACK_SIZE);
-	create_video_mapping();
+
 #ifdef HAS_DEVICE
 	create_video_mapping();
 #endif
